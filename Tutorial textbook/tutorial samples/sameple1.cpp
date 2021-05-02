7c193af28b56a304aec1b7a521ffa640fde98fd7
Xiangyu Ren <james_rxy@hotmail.com>
2021-05-02 15:04:17 -0700

    1

---
+++ Tutorial textbook/tutorial samples/sameple1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, sum;
+    cout << "Input 10 integers, press enter to finish" << endl;
+    cin >> a;
+    cin >> b;
+    sum = a + b;
+    cout << sum << endl;
+    return 0;
+}
